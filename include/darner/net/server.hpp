9c664f68352cd65398e21b7d73a3777442f51dd4
Erik Frey <erik@fawx.com>
2012-08-07 00:16:19 +0100

    separate net io_service and disk io_service

--- include/darner/net/server.hpp
+++ include/darner/net/server.hpp
@@ -1,8 +1,9 @@
 #ifndef __DARNER_SERVER_HPP__
 #define __DARNER_SERVER_HPP__
 
-#include <boost/bind.hpp>
+#include <boost/scoped_ptr.hpp>
 #include <boost/thread.hpp>
+#include <boost/bind.hpp>
 #include <boost/asio.hpp>
 
 #include "darner/util/log.h"
@@ -25,9 +26,10 @@ public:
           size_t num_workers)
    : listen_port_(listen_port),
      num_workers_(num_workers),
-     acceptor_(ios_),
-     strand_(ios_),
-     queues_(ios_, data_path)
+     disk_work_(new boost::asio::io_service::work(disk_ios_)),
+     acceptor_(net_ios_),
+     strand_(net_ios_),
+     queues_(disk_ios_, data_path)
    {
       // open the acceptor with the option to reuse the address (i.e. SO_REUSEADDR).
       boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::tcp::v4(), listen_port_);
@@ -37,26 +39,29 @@ public:
       acceptor_.listen();
 
       // get our first conn ready
-      handler_ = handler::ptr_type(new handler(ios_, parser_, queues_, stats_));
+      handler_ = handler::ptr_type(new handler(net_ios_, parser_, queues_, stats_));
 
       // pump the first async accept into the loop
       acceptor_.async_accept(handler_->socket(),
          boost::bind(&server::handle_accept, this, boost::asio::placeholders::error));
 
-      // spin up the threads that run the io service
+      // start up the disk service
+      disk_worker_ = boost::thread(boost::bind(&boost::asio::io_service::run, &disk_ios_));
+
+      // spin up the threads that run the handlers
       for (size_t i = 0; i != num_workers_; ++i)
-         workers_.create_thread(boost::bind(&boost::asio::io_service::run, &ios_));
+         net_workers_.create_thread(boost::bind(&boost::asio::io_service::run, &net_ios_));
    }
 
    void stop()
    {
       strand_.post(boost::bind(&server::handle_close, this));
-      // TODO: also close any idling clients, delete the work
-   }
+      // TODO: also close any idling clients
+      net_workers_.join_all();
 
-   void join()
-   {
-      workers_.join_all();      
+      // let the disk thread exit
+      disk_work_.reset();
+      disk_worker_.join();
    }
 
 private:
@@ -72,7 +77,7 @@ private:
 
       handler_->start();
 
-      handler_ = handler::ptr_type(new handler(ios_, parser_, queues_, stats_));
+      handler_ = handler::ptr_type(new handler(net_ios_, parser_, queues_, stats_));
       acceptor_.async_accept(handler_->socket(),
          strand_.wrap(
             boost::bind(&server::handle_accept, this, boost::asio::placeholders::error)));
@@ -86,14 +91,19 @@ private:
    unsigned short listen_port_;
    size_t num_workers_;
 
-   boost::asio::io_service ios_;
+   // net_ios_ runs all the handlers and the acceptor, disk_ios_ is exclusively for journal/disk i/o
+   // net_ios_ is safe for multiple threads
+   boost::asio::io_service net_ios_;
+   boost::asio::io_service disk_ios_;
+   boost::scoped_ptr<boost::asio::io_service::work> disk_work_;
    boost::asio::ip::tcp::acceptor acceptor_;
    boost::asio::strand strand_; // to avoid close() and async_accept firing at the same time
    queue_map queues_;
    request_parser parser_;
    stats stats_;
    handler::ptr_type handler_;
-   boost::thread_group workers_;
+   boost::thread_group net_workers_;
+   boost::thread disk_worker_;
 };
 
 } // darner
