5f19896f276d19ce48672f336615d4bf42c5a4d9
Erik Frey <erik@fawx.com>
2012-08-12 08:05:05 +0100

    first benchmark script

---
+++ bench/flood.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# flood test - try flood get/set at different concurrencies to measure throughput
+# kestrel is on port 22133
+# darner is on port 22134
+
+# kestrel reaches its best performance after a warmup period
+
+echo -n "warming up kestrel..."
+
+./db -p 22133 -s 100000 -g 100000 >/dev/null
+
+echo "done."
+
+# fill the queue with just a few items, to ensure there's always an item available
+echo -ne "flush db_bench\r\n" | nc localhost 22134 >/dev/null
+./db -p 22133 -s 512 -g 0 >/dev/null
+
+for i in 1 2 5 10 50 100 200 300
+do
+   echo -n "kestrel $i conns: "
+   ./db -p 22133 -s 100000 -g 100000 -c $i | grep -i "requests per second" | awk -F"    " '{print $2}'
+done
+
+echo -ne "flush db_bench\r\n" | nc localhost 22134 >/dev/null
+./db -p 22134 -s 512 -g 0 >/dev/null
+
+for i in 1 2 5 10 50 100 200 300 400 600 800 1000
+do
+   echo -ne "flush db_bench\r\n" | nc localhost 22134 >/dev/null
+   echo -n "darner $i conns: "
+   ./db -p 22134 -s 100000 -g 100000 -c $i | grep -i "requests per second" | awk -F"    " '{print $2}'
+done
+
+for i in 1 2 5 10 50 100 200 300 400 600 800 1000
+do
+   echo -n "memcache $i conns: "
+   ./db -p 11211 -s 100000 -g 100000 -c $i | grep -i "requests per second" | awk -F"    " '{print $2}'
+done
