25f9e24a643a1622d3ad9d75a3f018c71d5dd12e
Erik Frey <erik@fawx.com>
2012-08-16 08:09:48 +0100

    clean up iqstream/oqstream semantics
    - no more boost::optional
    - explicit opened/closed state for both streams
    - stream dtors handle closing/aborting
    - clearer documentation of queue class

--- include/darner/queue/iqstream.h
+++ include/darner/queue/iqstream.h
@@ -1,7 +1,7 @@
 #ifndef __DARNER_QUEUE_IQSTREAM_H__
 #define __DARNER_QUEUE_IQSTREAM_H__
 
-#include <boost/optional.hpp>
+#include <boost/shared_ptr.hpp>
 
 #include "darner/queue/queue.h"
 
@@ -12,20 +12,25 @@ class iqstream
 public:
 
    /*
-    * tries to open an item immediately.  reads will fail if it couldn't open an item.
+    * destroying an open iqstream will close it with erase = false
     */
-   iqstream(queue& _queue);
+   ~iqstream();
 
    /*
-    * on the first read, tries to fetch an item and returns true if one was available
+    * tries to open an item for reading.  returns true if an item was available.
+    */
+   bool open(boost::shared_ptr<queue> queue);
+
+   /*
+    * reads a chunk
     * can continue calling read until eof (until tell() == size()).
     */
-   bool read(std::string& result);
+   void read(std::string& result);
 
    /*
-    * closes the iqstream.  if remove, completes the pop of the item off the queue, otherwise returns it
+    * closes the iqstream.  if erase, completes the pop of the item off the queue, otherwise returns it.
     */
-   void close(bool remove);
+   void close(bool erase);
 
    /*
     * returns the position in the stream in bytes.  only valid after first read()
@@ -35,14 +40,14 @@ public:
    /*
     * returns the size of the item.  only valid after first read()
     */
-    queue::size_type size() const { return header_ ? header_->size : tell_; }
+    queue::size_type size() const { return header_.size; }
    
 private:
 
-   queue& queue_;
+   boost::shared_ptr<queue> queue_;
 
-   boost::optional<queue::id_type> id_; // id of key in queue, only set after a succesful first read
-   boost::optional<queue::header_type> header_; // only set if it's multi-chunk
+   queue::id_type id_; // id of key in queue, only valid if open() succeeded
+   queue::header_type header_; // only valid if it's multi-chunk
    queue::size_type chunk_pos_;
    queue::size_type tell_;
 };
